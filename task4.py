28680ae953599c766b94cd271b0557d1428cb3d6
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-11-13 12:48:20 -0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,4 +6,12 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
+import math
 
+a= float(input("what is the length of A side"))
+
+b= float(input("what is the length of B side"))
+
+c = sqrt(a**2 + b**2)
+
+print("The length of the hypotenuse is", c )
